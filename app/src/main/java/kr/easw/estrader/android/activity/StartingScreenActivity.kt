2a59783943cfe662cd9825e3bd679e7eb226d9b3
Supereru <superultrapowereru@gmail.com>
2023-03-19 00:44:07 +0900

    3월 18일 커밋 입찰대기화면(다이얼로그)

--- app/src/main/java/kr/easw/estrader/android/activity/StartingScreenActivity.kt
+++ app/src/main/java/kr/easw/estrader/android/activity/StartingScreenActivity.kt
@@ -1,12 +1,20 @@
 package kr.easw.estrader.android.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import kr.easw.estrader.android.R
 
 class StartingScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.fragment_login)
+        val imageButton = findViewById<Button>(R.id.btnNext)
+        imageButton.setOnClickListener {
+            val intent = Intent(applicationContext, TestActivity1::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
     }
 }
--- app/src/main/java/kr/easw/estrader/android/activity/TestActivity2.kt
+++ app/src/main/java/kr/easw/estrader/android/activity/TestActivity2.kt
@@ -1,7 +1,11 @@
 package kr.easw.estrader.android.activity
 
+import android.app.Dialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import kr.easw.estrader.android.R
 
 class TestActivity2 : AppCompatActivity() {
@@ -9,6 +13,23 @@ class TestActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test2)
 
+        val imageButton = findViewById<Button>(R.id.confirm_button)
+        imageButton.setOnClickListener {
+            val dialog = Dialog(this)
+            dialog.setContentView(R.layout.fragment_dialog)
+            dialog.show()
+        }}
+    fun onPositiveButtonClick(view: View) {
+        // Positive Button 클릭 시 처리할 작업 구현
+        val intent = Intent(applicationContext, TestActivity2::class.java)
+        startActivity(intent)
+    }
+
+    fun onNegativeButtonClick(view: View) {
+        // Negative Button 클릭 시 처리할 작업 구현
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
 
     }
 }
+
