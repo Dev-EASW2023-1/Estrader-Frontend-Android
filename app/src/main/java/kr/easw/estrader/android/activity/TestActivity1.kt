d224b38164b738531df67be9dbbc63c288107a07
Supereru <superultrapowereru@gmail.com>
2023-03-20 17:19:24 +0900

    3월 20일 커밋 다이얼로그

--- app/src/main/java/kr/easw/estrader/android/activity/TestActivity1.kt
+++ app/src/main/java/kr/easw/estrader/android/activity/TestActivity1.kt
@@ -5,18 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.airbnb.lottie.LottieAnimationView
 import kr.easw.estrader.android.R
+import kr.easw.estrader.android.fragment.AgentdelegationFragment
+import kr.easw.estrader.android.fragment.LoginFragment
+import kr.easw.estrader.android.fragment.RegisterFragment
 
 class TestActivity1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test1)
+        setContentView(R.layout.activity_mainlist)
 
-        val imageButton = findViewById<Button>(R.id.confirm_button2)
-        imageButton.setOnClickListener {
-            val intent = Intent(applicationContext, TestActivity2::class.java)
-            startActivity(intent)
-        }
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        val myFragment = AgentdelegationFragment()
+        val myFragment2 = RegisterFragment()
+        fragmentTransaction.add(kr.easw.estrader.android.R.id.mainlist_container_view, myFragment)
+        fragmentTransaction.commit()
+
     }
 }
---
+++ app/src/main/java/kr/easw/estrader/android/dialog/AwaitingbidDialog.kt
@@ -0,0 +1,43 @@
+package kr.easw.estrader.android.dialog
+
+import android.app.AlertDialog
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kr.easw.estrader.android.R
+import kr.easw.estrader.android.activity.TestActivity2
+
+class AwaitingbidDialog : AppCompatActivity() {
+    private lateinit var alertBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_awaitingbid)
+
+        // View inflate
+        alertBtn = findViewById(R.id.confirm_button)
+
+        // 기본 형태의 다이얼로그
+        alertBtn.setOnClickListener {
+            // 다이얼로그를 생성하기 위해 Builder 클래스 생성자를 이용해 줍니다.
+            val builder = AlertDialog.Builder(this)
+            // 다이얼로그 메시지 설정
+            builder.setMessage("전 화면으로 돌아갑니다.")
+            // Positive Button 클릭 시 처리할 작업 설정
+            builder.setPositiveButton("확인") { _, _ -> accept()
+            }
+            // 다이얼로그를 띄워주기
+            builder.show()
+        }
+    }
+
+    private fun accept() {
+        // Positive Button 클릭 시 처리할 작업 구현
+        val intent = Intent(applicationContext, kr.easw.estrader.android.activity.TestActivity1::class.java)
+        startActivity(intent)
+    }
+}
+
