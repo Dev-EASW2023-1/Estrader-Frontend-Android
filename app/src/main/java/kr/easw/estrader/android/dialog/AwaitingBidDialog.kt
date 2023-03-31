ce4c5a6d6f6c00b03b40c1612686cdf4ae4bc76c
Supereru <superultrapowereru@gmail.com>
2023-03-31 14:50:21 +0900

    3월 31일 화면전환시 진동 및 소리

--- app/src/main/java/kr/easw/estrader/android/dialog/AwaitingBidDialog.kt
+++ app/src/main/java/kr/easw/estrader/android/dialog/AwaitingBidDialog.kt
@@ -1,9 +1,12 @@
 package kr.easw.estrader.android.dialog
 
+import android.content.Context
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.os.Vibrator
 import androidx.appcompat.app.AppCompatActivity
 import kr.easw.estrader.android.R
 
@@ -15,15 +18,21 @@ import kr.easw.estrader.android.R
  */
 class AwaitingBidDialog : AppCompatActivity() {
     private lateinit var mHandler: Handler
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var vibrator: Vibrator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_awaitingbid)
 
         mHandler = Handler(Looper.getMainLooper())
+        mediaPlayer = MediaPlayer.create(this, R.raw.bingbong)
+        vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
 
         // 5초 뒤 SuccessDelegationDialog 이동
         mHandler.postDelayed({
+            mediaPlayer.start()
+            vibrator.vibrate(1000)
             startActivity(
                 Intent(this, SuccessDelegationDialog::class.java).apply {
                     flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
