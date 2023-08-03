bcc64b9d91f610bb2d34de38359a52f32f0512cf
Supereru <superultrapowereru@gmail.com>
2023-08-03 17:15:47 +0900

    8월 3일 BottomSheetDialog 임시적용

--- app/src/main/java/kr/easw/estrader/android/fragment/delegation/realtor/RealtorAwaitingFragment.kt
+++ app/src/main/java/kr/easw/estrader/android/fragment/delegation/realtor/RealtorAwaitingFragment.kt
@@ -1,18 +1,22 @@
 package kr.easw.estrader.android.fragment.delegation.realtor
 
-import android.content.Intent
+import android.annotation.SuppressLint
+import android.content.res.Resources
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kr.easw.estrader.android.R
-import kr.easw.estrader.android.activity.realtor.RealtorMainListActivity
 import kr.easw.estrader.android.databinding.FragmentRealtorAwaitingBinding
-import kr.easw.estrader.android.extensions.startActivity
+
 
 class RealtorAwaitingFragment : Fragment(), View.OnClickListener {
     private var _binding: FragmentRealtorAwaitingBinding? = null
@@ -51,12 +55,30 @@ class RealtorAwaitingFragment : Fragment(), View.OnClickListener {
         when(v.id){
             binding.fab.id -> anim()
             binding.fabHome.id -> {
-                requireActivity().startActivity<RealtorMainListActivity> {
-                    flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
-                }
-                requireActivity().finish()
+//                requireActivity().startActivity<RealtorMainListActivity> {
+//                    flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
+//                }
+//                requireActivity().finish()
+
+                showBottomSheetDialog()
+
             }
         }
+    }
+
+    private fun showBottomSheetDialog() {
+        val bottomSheetDialog = BottomSheetDialog(requireContext(), R.style.SlowBottomSheetDialog)
+        bottomSheetDialog.window?.attributes?.windowAnimations = R.style.SlowBottomSheetDialog
+        bottomSheetDialog.setContentView(R.layout.bottom_sheet_dialog_layout)
+
+        val parentLayout = bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+        parentLayout?.let {
+            val behavior = BottomSheetBehavior.from(it)
+            behavior.peekHeight = Resources.getSystem().displayMetrics.heightPixels
+            it.background = ContextCompat.getDrawable(it.context, R.drawable.rounded_dialog)
+        }
+
+        bottomSheetDialog.show()
     }
 
     private fun anim() {
