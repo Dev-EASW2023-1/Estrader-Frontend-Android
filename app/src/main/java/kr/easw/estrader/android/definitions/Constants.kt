07de0590397fe8da1c93185180b99beb5ca16183
JaeGyeong <fb_olddog@naver.com>
2023-09-07 21:45:49 +0900

    9월 7일 전역 상수 추가

--- app/src/main/java/kr/easw/estrader/android/definitions/Constants.kt
+++ app/src/main/java/kr/easw/estrader/android/definitions/Constants.kt
@@ -2,7 +2,7 @@ package kr.easw.estrader.android.definitions
 
 const val SERVER_URL = "http://172.17.0.10:8060"
 
-//const val SERVER_URL = "http://lab.dja.kr:19094"
+//const val SERVER_URL = "https://lab.simi.pe.kr"
 const val PREFERENCE_ID = "ID"
 const val PREFERENCE_PW = "PW"
 const val PREFERENCE_REALTOR_ID = "REALTORID"
@@ -13,3 +13,5 @@ const val PREFERENCE_PICTURE_URL = "PICTUREURL"
 const val PREFERENCE_CHECK = "CHECK"
 const val PREFERENCE_DOWNLOAD_ID = "DOWNLOADID"
 const val PDF_URL = "$SERVER_URL/contract/create-pdf"
+const val PREFERENCE_TOKEN = "TOKEN"
+const val PREFERENCE_REALTOR_TOKEN = "REALTORTOKEN"
