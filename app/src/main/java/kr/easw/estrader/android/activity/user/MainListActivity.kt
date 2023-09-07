b658c917bb3582acd58dd4c683b7e01b63346afe
Supereru <superultrapowereru@gmail.com>
2023-09-07 21:48:16 +0900

    9월 7일 사용자 메인 화면 종료할 때 다이얼로그 표시

--- app/src/main/java/kr/easw/estrader/android/activity/user/MainListActivity.kt
+++ app/src/main/java/kr/easw/estrader/android/activity/user/MainListActivity.kt
@@ -1,13 +1,18 @@
 package kr.easw.estrader.android.activity.user
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
 import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
+import kr.easw.estrader.android.R
 import kr.easw.estrader.android.databinding.ActivityMainlistBinding
 import kr.easw.estrader.android.fragment.user.MainListFragment
+import kr.easw.estrader.android.fragment.user.MapFragment
+
 
 /**
  * 사용자 전용 메인화면 activity
@@ -23,13 +28,56 @@ class MainListActivity : AppCompatActivity() {
         setContentView(activityBinding.root)
 
         callback = object : OnBackPressedCallback(true) {
+
+            //  R.id.navigation_home true 추가,R.id.navigation_home일때는 안되게 조건걹기
             override fun handleOnBackPressed() {
-                supportFragmentManager.commit {
-                    replace(activityBinding.framelayout.id, MainListFragment())
-                }
+
+                AlertDialog.Builder(this@MainListActivity)
+                    .setTitle("종료")
+                    .setMessage("종료하시겠습니까?")
+                    .setPositiveButton("확인") { _, _ ->
+                        val dialog = Dialog(this@MainListActivity)
+                        dialog.dismiss()
+                        finish()
+                    }
+                    .setNegativeButton("취소") { _, _ ->
+                    }
+                    .create()
+                    .show()
+
             }
+
+
         }
         this.onBackPressedDispatcher.addCallback(this, callback)
+        // 현재 호출된 버튼일때 또 누르면 호출 안되게 변경 -> 안하면 계속 쌓임
+        var currentSelectedItemId: Int? = null
+
+        activityBinding.mainlistNavigationView.setOnItemSelectedListener { item ->
+            if (currentSelectedItemId == item.itemId) return@setOnItemSelectedListener false
+
+            println("item :) | ${item.itemId}")
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    supportFragmentManager.commit {
+                        replace(activityBinding.framelayout.id, MainListFragment())
+                    }
+                    currentSelectedItemId = item.itemId
+                    true
+                }
+
+                R.id.navigation_map -> {
+                    supportFragmentManager.commit {
+                        replace(activityBinding.framelayout.id, MapFragment())
+                    }
+                    currentSelectedItemId = item.itemId
+                    true
+                }
+
+                else -> false
+            }
+        }
+
 
         // commit() 으로 Fragment Transaction 비동기 처리
         supportFragmentManager.commit {
@@ -44,6 +92,7 @@ class MainListActivity : AppCompatActivity() {
         imm.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
         return super.dispatchTouchEvent(event)
     }
+
     override fun onStop() {
         super.onStop()
     }
