f11e33d115b8d58d9d61aac6200e38f234d1ce74
namjaegyeong <fb_olddog@naver.com>
2023-12-01 17:12:15 +0900

    chore: 코드 정리

--- app/src/main/java/kr/easw/estrader/android/definitions/Constants.kt
+++ app/src/main/java/kr/easw/estrader/android/definitions/Constants.kt
@@ -1,8 +1,6 @@
 package kr.easw.estrader.android.definitions
 
 const val SERVER_URL = "http://172.17.0.10:8060"
-//const val SERVER_URL = "http://lab.dja.kr:19094"
-
 //const val SERVER_URL = "https://lab.simi.pe.kr"
 const val PREFERENCE_ID = "ID"
 const val PREFERENCE_PW = "PW"
@@ -16,6 +14,3 @@ const val PREFERENCE_DOWNLOAD_ID = "DOWNLOADID"
 const val PDF_URL = "$SERVER_URL/contract/create-pdf"
 const val PREFERENCE_TOKEN = "TOKEN"
 const val PREFERENCE_REALTOR_TOKEN = "REALTORTOKEN"
-
-const val Client_ID = "uc1k3vyemf"
-const val Client_Secret= "xAO2LAmQe1PD5AqQYn48imD2hPflQoyzUPmK1leU"
